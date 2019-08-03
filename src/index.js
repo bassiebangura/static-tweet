8963fb81f90d9dac18dda24d3c01e53ff02aa4b4
bassiebangura <devbassiebangura@gmail.com>
2019-08-02 22:52:06 -0400

    Add more css styling to tweet component elements.

--- src/index.js
+++ src/index.js
@@ -1,53 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import moment from "moment";
 import "./index.css";
 
 //Data for props
 const testTweet = {
   message: "Something about cats.",
-  gravatar: "xyz",
+  gravatar: "fe69ac45af41becd3a523f2b36d2bd08",
   author: { handle: "catperson", name: "IAMA Cat Person" },
   likes: 2,
-  retweets: 0,
+  retweets: 10,
   timestamp: "2016-07-30 21: 24: 37"
 };
 
 //Avatar Component
-function Avatar() {
-  return (
-    <img
-      src="https://www.gravatar.com/avatar/fe69ac45af41becd3a523f2b36d2bd08"
-      className="avatar"
-      alt="an avatar of Bassie Bangura"
-    />
-  );
+function Avatar({ hash }) {
+  let url = `https://www.gravatar.com/avatar/${hash}`;
+  return <img src={url} className="avatar" alt="an avatar of Bassie Bangura" />;
 }
 
 //Message Component
-function Message() {
-  return <div className="message">God has been faithful.</div>;
+function Message({ text }) {
+  return <div className="message">{text}</div>;
 }
 //NameWithHandle
-function NameWithHandle() {
+function NameWithHandle({ author }) {
+  const { name, handle } = author;
   return (
     <span className="name-with-handle">
-      <span className="name">Bassie Bangura</span>
-      <span className="handle">@programmer4Christ</span>
+      <span className="name">{name}</span>
+      <span className="handle">@{handle}</span>
     </span>
   );
 }
 
 //Time Component
-const Time = () => <span className=" time"> 3h ago </span>;
+const Time = ({ time }) => {
+  const timeString = moment(time).fromNow();
+  return <span className=" time">{timeString}</span>;
+};
+
+//Helper function
+function getRetweetCount(count) {
+  if (count > 0) {
+    return <span className="retweet-count">{count}</span>;
+  } else {
+    return null;
+  }
+}
 
 //ReplyButton Component
-const ReplyButton = () => <i className=" fa fa-reply reply-button" />;
+const ReplyButton = ({ count }) => <i className=" fa fa-reply reply-button" />;
 
 //RetweetButton Component.
-const RetweetButton = () => <i className=" fa fa-retweet retweet-button" />;
+const RetweetButton = ({ count }) => {
+  return (
+    <span className="retweet-button">
+      <i className=" fa fa-retweet retweet-button" />
+      {getRetweetCount(count)}
+    </span>
+  );
+};
 
 //LikeButton Component
-const LikeButton = () => <i className=" fa fa-heart like-button" />;
+const LikeButton = ({ count }) => {
+  return (
+    <span className="like-button">
+      <i className=" fa fa-heart like-button" />
+      {count > 0 && <span className="like-count">{count}</span>}
+    </span>
+  );
+};
 
 //MoreOptionsButton
 const MoreOptionsButton = () => (
@@ -55,18 +78,18 @@ const MoreOptionsButton = () => (
 );
 
 //Tweet Component
-function Tweet(tweet) {
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <Avatar />
+      <Avatar hash={tweet.gravatar} />
       <div className="content">
-        <NameWithHandle />
-        <Time />
-        <Message />
+        <NameWithHandle author={tweet.author} />
+        <Time time={tweet.timestamp} />
+        <Message text={tweet.message} />
         <div className="buttons">
           <ReplyButton />
-          <RetweetButton />
-          <LikeButton />
+          <RetweetButton count={tweet.retweets} />
+          <LikeButton count={tweet.likes} />
           <MoreOptionsButton />
         </div>
       </div>
@@ -74,4 +97,4 @@ function Tweet(tweet) {
   );
 }
 
-ReactDOM.render(<Tweet tweet = {testTweet}/>, document.getElementById("root"));
+ReactDOM.render(<Tweet tweet={testTweet} />, document.getElementById("root"));
